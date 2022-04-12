cd494e045770fa071b38aca2d32ee91620e87f3c
yzforum1 <103417658+yzforum1@users.noreply.github.com>
2022-04-12 13:47:47 +0800

    Update and rename JSClickBubble.user.js to user.js

--- user.js
+++ user.js
@@ -9,10 +9,11 @@
 // @run-at       document_start
 // ==/UserScript==
 onload = function() {
-    var click_cnt = 0;
+    var arr=["OωO","(๑•́ ∀ •̀๑)","(๑•́ ₃ •̀๑)","(๑•̀_•́๑)","（￣へ￣）","(╯°口°)╯(┴—┴","૮( ᵒ̌皿ᵒ̌ )ა","╮(｡>口<｡)╭","(ꐦ°᷄д°᷅)","❤","👽","👾","🎃","😺","😸","😹","🙏","😉","😌","😇","😊","😎"]
     var $html = document.getElementsByTagName("html")[0];
     var $body = document.getElementsByTagName("body")[0];
     $html.onclick = function(e) {
+	var n = Math.round(Math.random()*arr.length);
         var $elem = document.createElement("b");
         $elem.style.color = "#E94F06";
         $elem.style.zIndex = 9999;
@@ -23,50 +24,7 @@ onload = function() {
         $elem.style.left = (x - 10) + "px";
         $elem.style.top = (y - 20) + "px";
         clearInterval(anim);
-        switch (++click_cnt) {
-            case 10:
-                $elem.innerText = "OωO";
-                break;
-            case 20:
-                $elem.innerText = "(๑•́ ∀ •̀๑)";
-                break;
-            case 30:
-                $elem.innerText = "(๑•́ ₃ •̀๑)";
-                break;
-            case 40:
-                $elem.innerText = "(๑•̀_•́๑)";
-                break;
-            case 50:
-                $elem.innerText = "（￣へ￣）";
-                break;
-            case 60:
-                $elem.innerText = "(╯°口°)╯(┴—┴";
-                break;
-            case 70:
-                $elem.innerText = "૮( ᵒ̌皿ᵒ̌ )ა";
-                break;
-            case 80:
-                $elem.innerText = "╮(｡>口<｡)╭";
-                break;
-            case 90:
-                $elem.innerText = "( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃";
-                break;
-            case 100:
-            case 101:
-            case 102:
-            case 103:
-            case 104:
-            case 105:
-                $elem.innerText = "(ꐦ°᷄д°᷅)";
-                break;
-	    case 120:
-		click_cnt = 0;
-		break;
-            default:
-		// 手动更换下面这行双引号里面的内容 如"😀"
-                $elem.innerText = "❤";
-                break;
-        }
+	$elem.innerText = arr[n]
         $elem.style.fontSize = Math.random() * 10 + 8 + "px";
         var increase = 0;
         var anim;
